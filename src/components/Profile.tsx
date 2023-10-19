9117ad1d3c0e91f2059d42bb3d5b5fcf7f4a21c2
BearHumanS <bye0651@naver.com>
2023-10-19 13:55:56 +0900

    FEAT: profile page 생성 및 스타일링

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,68 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <>
+      <ProfileBox>
+        <ProfileFlex>
+          <ProfileImage />
+          <ProfileUserInfo>
+            <ProfileEmail>test@test.com</ProfileEmail>
+            <ProfileUserName>유저저</ProfileUserName>
+          </ProfileUserInfo>
+        </ProfileFlex>
+        <ProfileLogout to="/">로그아웃</ProfileLogout>
+      </ProfileBox>
+    </>
+  );
 };
+
+const ProfileBox = styled.div`
+  display: flex;
+  gap: 18px;
+  align-items: center;
+  font-size: 18px;
+  margin: 0 auto;
+  max-width: 680px;
+  text-align: left;
+  line-height: 24px;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const ProfileFlex = styled.div`
+  display: flex;
+  gap: 18px;
+  align-items: center;
+`;
+
+const ProfileImage = styled.div`
+  width: 72px;
+  height: 72px;
+  background-color: #f2f2f2;
+  border-radius: 50%;
+`;
+
+const ProfileUserInfo = styled.div``;
+
+const ProfileEmail = styled.div`
+  font-weight: 500;
+`;
+
+const ProfileUserName = styled.div`
+  font-size: 16px;
+  padding: 4px 0;
+`;
+
+const ProfileLogout = styled(Link)`
+  color: gray;
+  font-size: 14px;
+
+  &:hover,
+  &:focus {
+    color: black;
+  }
+`;
 
 export default Profile;
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -7,19 +7,22 @@ import EditPostPage from '@/pages/posts/editPost';
 import ProfilePage from '@/pages/profile';
 import LoginPage from '@/pages/login';
 import SignUpPage from '@/pages/signup';
+import Layout from './Layout';
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/posts" element={<PostList />} />
-      <Route path="/posts/:id" element={<PostDetailPage />} />
-      <Route path="/posts/new" element={<NewPostPage />} />
-      <Route path="/posts/edit/:id" element={<EditPostPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="*" element={<Navigate replace to="/" />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<PostList />} />
+        <Route path="/posts/:id" element={<PostDetailPage />} />
+        <Route path="/posts/new" element={<NewPostPage />} />
+        <Route path="/posts/edit/:id" element={<EditPostPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Route>
     </Routes>
   );
 };
