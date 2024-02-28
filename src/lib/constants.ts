cefdc82de8104830a88639b9852f4daea3de427d
BearHumanS <bye0651@naver.com>
2024-02-29 03:11:31 +0900

    Feat: 카테고리 목록 추가

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -12,8 +12,8 @@ export const EMAIL_REGEX =
 /* export const PASSWORD_REGEX =
   /^(?=.*\d)(?=.*[!@#$%^&*()-+=])(?=.*[a-zA-Z]).{8,16}$/; */
 
-export type CategoryType = 'free' | '면접질문' | '스터디' | '예시3';
-export const CATEGORYS: CategoryType[] = ['면접질문', '스터디', '예시3'];
+export type CategoryType = 'free' | '면접질문' | '스터디' | '회고';
+export const CATEGORYS: CategoryType[] = ['면접질문', '스터디', '회고'];
 
 export const useAdmin = () => {
   const { user } = useContext(AuthContext);
@@ -25,3 +25,5 @@ export const useAdmin = () => {
 
   return isAdmin;
 };
+
+export const PROFILE_DEFAULT_IMG = '/src/assets/default_profile.svg';
